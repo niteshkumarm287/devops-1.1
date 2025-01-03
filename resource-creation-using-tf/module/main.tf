d4ba8b91deb0a1080e95f707c805bf187b24ed92
Nitesh kumar <kumarmnitesh@gmail.com>
2025-01-03 10:51:12 +0530

    deleted unwanted files

--- resource-creation-using-tf/module/main.tf
+++
@@ -1,6 +0,0 @@
-resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
-  initial_node_count = var.initial_node_count
-  deletion_protection = false
-}
