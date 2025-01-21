19782aca025cce4220bfa6e022b46aa807a8a7e2
Nitesh kumar <kumarmnitesh@gmail.com>
2025-01-21 09:10:25 +0530

    updated file

--- GCP-cloud-build/Docker/main.py
+++ GCP-cloud-build/Docker/main.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<div style=\"text-align: center;\"><h1>Hello from Tanmay!</h1><p>Welcome to GCP DevOps Course.</p><p></p></div>'
-
+    return '<div style="text-align: center;"><h1>Hello from Nitesh!</h1><p>Welcome to GCP DevOps Course.</p><p></p></div>'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Bind Flask to all interfaces (0.0.0.0)
+    app.run(debug=True, host="0.0.0.0", port=8080)
